6e2e732647bfc1e32f81ae86e9d9f7c2755978a2
Venkatesh3686 <venkateshpentakota888@gmail.com>
2025-10-02 12:46:44 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-full flex justify-center items-center">
+      <Button>Click me!</Button>
+    </div>
+  );
+};
+
+export default Home;
