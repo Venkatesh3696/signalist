21bc80a564d76f89ff6ce921b52ea0170d4d24a6
Venkatesh3686 <venkateshpentakota888@gmail.com>
2025-10-05 23:49:38 +0530

    add home tradingview widget

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
 import Header from "@/components/Header";
+import TradingviewWidget from "@/components/TradingviewWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-      <Header />
-      <h1>Home</h1>
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:colspan-1">
+          <TradingviewWidget
+            title="Markets Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custon-chart"
+          />
+        </div>
+      </section>
     </div>
   );
 };
